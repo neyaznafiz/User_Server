6bfce3649efc31700c5ff5baed75a104cd41a913
neyaznafiz <niiyazobug@gmail.com>
2022-09-03 17:12:13 +0600

    property error solved and patch api

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,6 +1,16 @@
 let user = require('../public/user.json')
 
 
+// get random api
+module.exports.getRandomUser = (req, res) => {
+
+    res.status(200).json({
+        success: true,
+        message: 'success',
+        data: user[Math.floor(Math.random() * (user.length + 1))]
+    })
+}
+
 // get all user and limit user api controller
 module.exports.getAllUser = (req, res,) => {
     const { limit } = req.query
@@ -17,21 +27,68 @@ module.exports.getAllUser = (req, res,) => {
 }
 
 
-// save/post user api controller
-module.exports.saveAUser = (req, res) => {
-    user.push(req.body)
+
+// filter by id
+module.exports.getUserById = (req, res) => {
+    const { id } = req.params
+    const foundUser = user.find(user => user.id === Number(id))
     res.status(200).send({
         success: true,
         message: 'success',
-        data: user
+        data: foundUser
     })
+}
+
+
+// save/post user api controller
+module.exports.saveAUser = (req, res) => {
+    const { id, name, gender, contact, address, photoUrl } = req.body
+
+
+    if (id && name && gender && contact && address && photoUrl) {
+        user.push(req.body)
+        res.status(200).send({
+            success: true,
+            message: 'success',
+            data: user
+        })
+    }
+    else {
+      res.send({
+        success: false,
+        error: "Missing property."
+    })
+    }
     res.status(500).send({
         success: false,
         error: 'Internal server error.'
     })
 }
 
-// user delete
+// update user api controller
+module.exports.updateUser = (req, res) => {
+    const newData = req.body
+    const { id } = req.params
+    const filter = { _id: id }
+
+    const oldData = user.find(user => user.id === Number(id))
+
+    // newData.id = id
+    // newData.name = req.body.name
+    // newData.gender = req.body.gender
+    // newData.contact = req.body.contact
+    // newData.address = req.body.address
+    // newData.photoUrl = req.body.photoUrl
+
+    res.status(200).send({
+        success: true,
+        message: 'success',
+        data: { ...oldData, ...newData }
+    })
+}
+
+
+// user delete api controller
 module.exports.deleteUser = (req, res) => {
     const { id } = req.params
     const filter = { _id: id }
--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -4,12 +4,18 @@ const userController = require("../../controller/user.controller")
 const router = express.Router()
 
 router
+    .route('/random')
+    .get(userController.getRandomUser)
+
+router
     .route('/')
     .get(userController.getAllUser)
     .post(userController.saveAUser)
 
 router
     .route('/:id')
+    .get(userController.getUserById)
+    .patch(userController.updateUser)
     .delete(userController.deleteUser)
 
 
